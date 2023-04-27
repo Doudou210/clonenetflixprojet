b8891c248886c37747ce3345a40ce3b66bd4677c
Doudou210 <siakakoudou.doumbia@yahoo.com>
2023-04-27 15:51:22 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,54 +1,25 @@
-import { useEffect, useState } from "react";
-import ApiMovie from "./ApiMovie";
-import FeaturedMovie from "./components/FeaturedMovie";
-import Header from "./components/Header";
-import MovieSection from "./components/MovieSection";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+// import Home from "./compontents/Home";
 import "./App.css";
+// import "./Header.css";
+// import "./FeaturedMovie.css";
+// import "./MovieSection";
+// import MovieSection from "./compontents/MovieSection";
+import Home1 from "./compontents/Home1";
+import Details from "./compontents/Details"
 
 
 function App() {
-  const [moviesList, setMoviesList] = useState([])
-  const [featuredData, setfeaturedData] = useState(null)
-  
-  
-    useEffect(() => {
-    const loadAllMovies = async () => {
-    //liste de tous les film
-      let list = await ApiMovie.getHomeMovies() 
-      setMoviesList(list)
-
-    //un seul film a l'affiche
-      let originals = list.filter((oneMovie) => oneMovie.slug === "top-rated")
-      
-      let chooseRandomMovie = Math.floor(
-        Math.random() * (originals[0].items.results.length - 1)
-      )
-      let chosen = originals[0].items.results[chooseRandomMovie]
-      let chosenInfo = await ApiMovie.getyMovieInfo(chosen.id, "movie")
-      setfeaturedData(chosenInfo)
-     
-    }
-    
-    loadAllMovies()
-    
-
-  }, [])
-
-
   return (
-    <div className="page">
-    <Header />
-    {featuredData && <FeaturedMovie films={featuredData} />}
-    <section className="lists">
-      {moviesList.map((item, key) => (
-      <MovieSection key={key} title={item.title} items={item.items} />
-    
-  ))}
-  
-  
-</section>
-</div>
+      
+      <Routes>
+        <Route path="/" element={<Home1/>}/>
+        <Route path='/home/details' Component={Details}/>
 
-  )}
+        {/* <Route path="/page" element={<MovieSection/>}/> */}
+      </Routes>
+  );
+}
 
 export default App;
